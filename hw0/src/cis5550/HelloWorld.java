d5d8f1bfb8b82fa129db8f1873b2812878cccae9
margolisj <jmarg@sas.upenn.edu>
2025-09-03 00:04:31 -0400

    Adds HW0

---
+++ hw0/src/cis5550/HelloWorld.java
@@ -0,0 +1,8 @@
+package cis5550;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Your program's logic goes here
+        System.out.println("Hello World (Joshua Margolis)"); 
+    }
+}
